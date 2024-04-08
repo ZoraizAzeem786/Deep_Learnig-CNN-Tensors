fc151ba589cf153627fbe409680ce20a615825c7
faiza-khatri <161750227+faiza-khatri@users.noreply.github.com>
2024-04-08 18:17:25 +0500

    Update tensorImplementation.py
    added shape method

--- tensorImplementation.py
+++ tensorImplementation.py
@@ -1,5 +1,25 @@
 class Tensor:
     def __init__(self, data):
         self.data = data
+    def shape(self):
+        if self.data == []:
+            return ()
+        elif isinstance(self.data, int):
+            return (1,)
+        shape = [len(self.data)]
+        temp = self.data[0]
+
+        while isinstance(temp, list):
+            shape.append(len(temp))
+            # Check if all rows have the same length
+            if any(len(row) != len(temp) for row in self.data):
+                return "Invalid tensor shape"
+            temp = temp[0]
+
+        if len(shape) > 1:
+            return tuple(shape)
+        else:
+            return (shape[0],)
+
     
     
